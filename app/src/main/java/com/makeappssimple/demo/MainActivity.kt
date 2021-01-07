77e337b81dc5a310d43dd5b19147e990bb7dd29a
Abhimanyu <abhimanyu.n14@gmail.com>
2021-01-07 14:42:49 +0530

    Starter code

--- app/src/main/java/com/makeappssimple/demo/MainActivity.kt
+++ app/src/main/java/com/makeappssimple/demo/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.makeappssimple.demo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    var count = 0
+    lateinit var textview: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textview = findViewById(R.id.textview_count)
+
+        findViewById<Button>(R.id.button_increment).setOnClickListener {
+            count++
+            textview.text = count.toString()
+        }
+
+        findViewById<Button>(R.id.button_decrement).setOnClickListener {
+            count--
+            textview.text = count.toString()
+        }
     }
 }
